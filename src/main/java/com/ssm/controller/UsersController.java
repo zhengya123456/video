a0be8611b93f190df4ab221e8dd65be24b43202c
zhengya123456 <2509409959@qq.com>
2020-06-10 16:54:38 +0800

    更新

--- src/main/java/com/ssm/controller/UsersController.java
+++ src/main/java/com/ssm/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.ssm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsersController {
@@ -11,4 +12,11 @@ public class UsersController {
         return "detail";
     }
 
+    @RequestMapping("/new")
+    public ModelAndView getNew(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("new");
+        modelAndView.addObject("message",100);
+        return modelAndView;
+    }
 }
